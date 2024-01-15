f7f8ef220f3c996d2527751a1ba17fab364c34cb
Lucas-Buk <lucasbukcardoso@gmail.com>
2024-01-15 10:12:45 -0300

    Adding documentation

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# import sphinx_rtd_theme
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Estudo Câncer Colorretal'
+copyright = '2023, Lucas Buk Cardoso'
+author = 'Lucas Buk Cardoso'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['nbsphinx',
+    'sphinx.ext.mathjax',
+]
+
+nbsphinx_allow_errors = True
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+####html_logo = 'images/image.png'
+
+html_title = 'Documentação do estudo de câncer colorretal'
+html_short_title = 'Câncer Colorretal'
